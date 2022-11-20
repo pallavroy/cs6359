00fe9c5d74d8bd08b613b83032833d0e8380673b
Kumar Pallav <kumarpallav@Kumars-Air.attlocal.net>
2022-11-19 21:32:52 -0600

    Fix method signature to work with float average

--- src/main/java/com/utd/cs6359/assignment4/Average.java
+++ src/main/java/com/utd/cs6359/assignment4/Average.java
@@ -1,8 +1,8 @@
 package com.utd.cs6359.assignment4;
 
 public class Average {
-    public int average(int k, int[] list) {
-        int average = 0;
+    public float average(int k, int[] list) {
+        float average = 0.0f;
         int n = Math.min(k, list.length);
         if (n>0) {
             for (int i=0; i<n; i++) {
--- src/test/java/com/utd/cs6359/assignment4/AverageTest.java
+++ src/test/java/com/utd/cs6359/assignment4/AverageTest.java
@@ -8,48 +8,48 @@ public class AverageTest extends TestCase {
         Average avg = new Average();
         int[] num = {1, 2, 3};
         int k = 3;
-        assertEquals(2, avg.average(k, num));
+        assertEquals(2f, avg.average(k, num));
     }
 
     public void testAverage_whenKZero_emptyList() {
         Average avg = new Average();
         int[] num = {};
         int k = 0;
-        assertEquals(0, avg.average(k, num));
+        assertEquals(0f, avg.average(k, num));
     }
 
     public void testAverage_whenKZero_nonEmptyList() {
         Average avg = new Average();
         int[] num = {1, 2, 3};
         int k = 0;
-        assertEquals(0, avg.average(k, num));
+        assertEquals(0f, avg.average(k, num));
     }
 
     public void testAverage_whenKNegative_nonEmptyList() {
         Average avg = new Average();
         int[] num = {1, 2, 3};
         int k = -1;
-        assertEquals(0, avg.average(k, num));
+        assertEquals(0f, avg.average(k, num));
     }
 
     public void testAverage_whenKLessThanListLength_nonEmptyList() {
         Average avg = new Average();
         int[] num = {1, 2, 3, 4, 5};
         int k = 3;
-        assertEquals(2, avg.average(k, num));
+        assertEquals(2f, avg.average(k, num));
     }
 
     public void testAverage_whenKGreaterThanListLength_nonEmptyList() {
         Average avg = new Average();
         int[] num = {1, 2, 3, 4, 5};
         int k = 8;
-        assertEquals(3, avg.average(k, num));
+        assertEquals(3f, avg.average(k, num));
     }
 
     public void testAverage_floatAvg() {
         Average avg = new Average();
         int[] num = {1, 2, 3, 4};
         int k = 5;
-        assertEquals(2.5, avg.average(k, num));
+        assertEquals(2.5f, avg.average(k, num));
     }
 }
